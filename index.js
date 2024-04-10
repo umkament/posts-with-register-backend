a5726013e91dfdbb5c5efc5c9037b34d01412c79
umkament <97944243+umkament@users.noreply.github.com>
2024-04-10 16:48:39 +0300

    fix add image for post

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ app.get('/auth/me', checkAuth, UserController.getMe);
 
 // app.post('/upload', checkAuth, upload.single('image'), (req, res) => {
 //   res.json({
-//     url: `/uploads/${req.file.originalname}`,
+//     url: `/uploads/${req.file.originalName}`,
 //   });
 // });
 
