2252555e56e509c2aa1b9614f3b4d68675d905c8
umkament <97944243+umkament@users.noreply.github.com>
2024-04-13 12:24:54 +0300

    fix error (const imageData = req.body.image)

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 import express from 'express';
 import fs from 'fs';
-//import multer from 'multer';
+import multer from 'multer';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import { v4 as uuidv4 } from 'uuid';
+// import { v4 as uuidv4 } from 'uuid';
 
 const PORT = process.env.PORT || 4441
 
@@ -24,20 +24,20 @@ mongoose
 
 const app = express();
 
-// const storage = multer.diskStorage({
-//   destination: (_, __, cb) => {
-//     if (!fs.existsSync('uploads')) {
-//       fs.mkdirSync('uploads');
-//     }
-//     cb(null, 'uploads');
-//   },
-//   filename: (_, file, cb) => {
-//     cb(null, file.originalname);
-//   },
-// });
-// const upload = multer({ storage });
+const storage = multer.diskStorage({
+  destination: (_, __, cb) => {
+    if (!fs.existsSync('uploads')) {
+      fs.mkdirSync('uploads');
+    }
+    cb(null, 'uploads');
+  },
+  filename: (_, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
+const upload = multer({ storage });
 
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
 app.use(cors());
 
 app.use('/uploads', express.static('uploads'));
@@ -46,14 +46,14 @@ app.post('/auth/login', loginValidation, handleValidationErrors, UserController.
 app.post('/auth/register', registerValidation, handleValidationErrors, UserController.register);
 app.get('/auth/me', checkAuth, UserController.getMe);
 
-// app.post('/upload', checkAuth, upload.single('image'), (req, res) => {
-//   res.json({
-//     url: `/uploads/${req.file.originalName}`,
-//   });
-// });
+app.post('/upload', checkAuth, upload.single('image'), (req, res) => {
+  res.json({
+    url: `/uploads/${req.file.originalName}`,
+  });
+});
 
 
-app.post('/upload', checkAuth, (req, res)=>{
+/*app.post('/upload', checkAuth, (req, res)=>{
 
 const imageData = req.body.image
    const fileName = uuidv4()+'.jpg'
@@ -72,7 +72,7 @@ const imageData = req.body.image
        url: `/uploads/${fileName}`
      });
    });
- })
+ })*/
 
 
 app.get('/tags', PostController.getLastTags);
