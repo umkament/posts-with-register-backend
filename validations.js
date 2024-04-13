2f1a952f881db1f612818eaba1d81177fbcf0a81
umkament <97944243+umkament@users.noreply.github.com>
2024-04-13 12:39:56 +0300

    change validations isString() on isURL()

--- validations.js
+++ validations.js
@@ -17,5 +17,5 @@ export const postCreateValidation = [
   body('title', 'Введите заголовок статьи').isLength({ min: 3 }).isString(),
   body('text', 'Введите текст статьи').isLength({ min: 3 }).isString(),
   body('tags', 'Неверный формат тэгов').optional().isArray(),
-  body('imageUrl', 'Неверная ссылка на изображение').optional().isString(),
+  body('imageUrl', 'Неверная ссылка на изображение').optional().isURL(),
 ];
